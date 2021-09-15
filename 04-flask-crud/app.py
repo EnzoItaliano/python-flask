6588aa0e1a8c41f6b8455498d636e77ed2a96241
Enzo Italiano <enzo.ditaliano@gmail.com>
2021-09-15 18:01:21 -0300

    Corrige dois testes que estavam falhando

--- 04-flask-crud/app.py
+++ 04-flask-crud/app.py
@@ -5,6 +5,10 @@ from snack import Snack
 app = Flask(__name__)
 snack_list = []
 
+@app.route('/')
+def home():
+    return redirect(url_for('index'))
+
 @app.route("/snacks", methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
@@ -17,18 +21,20 @@ def index():
 def add_snack():
     return render_template('add-snack.html')
 
-@app.route("/snacks/<int:id>", methods=['PATCH', 'GET', 'DELETE'])
+@app.route("/snacks/<int:id>", methods=['POST', 'GET'])
 def show_snack(id):
     index_snack = -1
-    if request.method == 'PATCH':
-        snack_list[index_snack].name = request.form['name']
-        snack_list[index_snack].kind = request.form['kind']
-    elif request.method == 'DELETE':
-        snack_list.remove(snack_list[index_snack])
-        return redirect(url_for('index'))
     for index, snack in enumerate(snack_list):
         if snack.id == id:
             index_snack = index
+    if 'PATCH' in str(request.query_string):
+        snack_list[index_snack].name = request.form['name']
+        snack_list[index_snack].kind = request.form['kind']
+        return redirect(url_for('index'))
+    elif 'DELETE' in str(request.query_string):
+        snack_list.remove(snack_list[index_snack])
+        return redirect(url_for('index'))
+    
     return render_template('snack.html', snack=snack_list[index_snack])
 
 @app.route("/snacks/<int:id>/edit")
